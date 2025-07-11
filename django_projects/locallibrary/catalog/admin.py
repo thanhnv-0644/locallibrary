e83bd209fea5273e4116544ea6541053b679a74d
Nguyễn Việt Thành <nvt220104@gmail.com>
2025-07-11 17:47:32 +0700

    Django Tutorial Part 4

--- django_projects/locallibrary/catalog/admin.py
+++ django_projects/locallibrary/catalog/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance
 
-# Register your models here.
+admin.site.register(Genre)
+
+# Định nghĩa lớp quản trị
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = (
+        'last_name',
+        'first_name',
+        'date_of_birth',
+        'date_of_death',
+    )
+    fields = [
+        'first_name',
+        'last_name',
+        ('date_of_birth', 'date_of_death')
+    ]
+
+
+# Đăng ký lớp quản trị với model tương ứng
+admin.site.register(Author, AuthorAdmin)
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+# Đăng ký lớp quản trị cho Book bằng decorator
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+
+# Đăng ký lớp quản trị cho BookInstance bằng decorator
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+
