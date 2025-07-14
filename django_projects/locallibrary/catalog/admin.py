f1a583b9c3bc175ef98f21115b33393c0bfa86f9
Nguyễn Việt Thành <nvt220104@gmail.com>
2025-07-14 16:26:54 +0700

    Django Tutorial Part 5

--- django_projects/locallibrary/catalog/admin.py
+++ django_projects/locallibrary/catalog/admin.py
@@ -49,5 +49,3 @@ class BookInstanceAdmin(admin.ModelAdmin):
             'fields': ('status', 'due_back')
         }),
     )
-
-
--- django_projects/locallibrary/catalog/views.py
+++ django_projects/locallibrary/catalog/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
 # Create your views here.
+from catalog.models import Book, Author, BookInstance, Genre
+from catalog.constants import LoanStatus
+
+
+def index(request):
+    """Hàm view cho trang chủ của website."""
+
+    # Tạo thống kê số lượng cho một số đối tượng chính
+    num_books = Book.objects.count()
+    num_instances = BookInstance.objects.count()
+    
+    # Sách có sẵn (status = 'a')
+    num_instances_available = BookInstance.objects.filter(
+        status=LoanStatus.AVAILABLE.value
+    )
+
+# Hàm all() được ngầm định mặc định
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+# Trả về file HTML template index.html với dữ liệu trong biến context
+    return render(request, 'index.html', context=context)
